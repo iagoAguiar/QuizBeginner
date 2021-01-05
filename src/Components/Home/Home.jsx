be1f11cc37d0e0990f45816ef5515648266ed3fb
iagoAguiar <iagoaguiar202@gmail.com>
2021-01-05 13:56:01 -0300

    'teste'

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import "./Home.css"
 
-import {  Fab  } from '@material-ui/core';
+import {  Button  } from '@material-ui/core';
 
 
 export default Home =>
      <div className="Home">
           <div className="Titulo">
                
-               //teste
+               
                <h1 className="texto_titulo"> Quiz Sobre Iago: </h1>
           </div>
-          <Fab className="botao" href="Questions" variant="extended" color="primary" aria-label="add">Start</Fab>
+
+          <Button variant="contained" color="primary" href="Questions">Start </Button>
+
+
      </div>
 
 
